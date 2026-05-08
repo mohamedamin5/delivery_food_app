051c352a82fe9b7690e15c2325b21616f437f42f
Moahmed <mohamed.amin.flutter@gnail.com>
2026-05-08 12:00:37 +0300

    feat: implement secure storage data source and fix import paths

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter_application_2/core/data/data_source/app_local_data_sourc
 import 'package:flutter_application_2/core/id/service_locator.dart';
 import 'package:flutter_application_2/core/localization/app_localizations_delegate.dart';
 import 'package:flutter_application_2/blocs_imports.dart';
-import 'package:flutter_application_2/core/data/data_source/secure_storage_data_sourcelmpl.dart';
+import 'package:flutter_application_2/core/data/data_source/secure_storage_data_sourceimpl.dart';
 import 'package:flutter_application_2/features/splashscreens/logic/splash_bloc.dart';
 
 import 'package:flutter_application_2/screens.dart';
